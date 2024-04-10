7add39a344ac1389f53a2b3b24127426f8b69e81
huan figuereido <huanfiguereido@gmail.com>
2024-04-10 20:55:14 -0300

    wip :coffe: /finish the layout of login screen

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # chainsawman
+
+- preciso criar um banco de dados onde posso salvar um usuário e uma senha para que seja feita uma verificação com a tela de login que já está construida em app
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
+import React, { useState, useRef, useEffect } from "react";
 
+/*components>>>*/
+
+/*<<<components*/
 import './App.scss'
 
 function App() {
 
+  const[userData, setUserData] = useState([]);
+  const user = useRef(null);
+  const password = useRef(null);
+  const userLabel = useRef(null);
+  const passwordLabel = useRef(null);
+
+  function CheckUserAnPassword(event){
+    user.current.value.length < 6 ? userLabel.current.style.display="flex": userLabel.current.style.display="none";
+    password.current.value.length < 8 ? passwordLabel.current.style.display="flex" : passwordLabel.current.style.display="none";
+    event.preventDefault();
+  }
+
   return (
     <main>
-      <h1>oi</h1>
+      <aside></aside>
+      <form>
+        <h1>entrar</h1>
+        <label htmlFor="user" ref={ userLabel }>entre com um usuário válido</label>
+        <input type="text" placeholder="nome de usuário" ref={ user} />
+        <label htmlFor="password" ref={ passwordLabel }>senha incorreta</label>
+        <input type="password" placeholder="sua senha" ref={ password }/>
+        <p>esqueçeu a sua senha?</p>
+        <input type="submit" value="entrar" onClick={ CheckUserAnPassword }/>
+        <p>não tem uma conta? registre-se</p>
+        <button type="button">entrar como convidado</button>{/*teste sem verificação de dados*/}
+      </form>
     </main>
   )
 }
