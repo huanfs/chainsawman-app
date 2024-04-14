d78a85c921621451fec5c216d4ce833d4cd5e427
huan figuereido <huanfiguereido@gmail.com>
2024-04-14 16:03:48 -0300

    feat :smile: /now,correctly checks if user and passows are saved in local storage to proceed with logIn

--- src/components/enter/Enter.jsx
+++ src/components/enter/Enter.jsx
@@ -21,29 +21,6 @@ const Enter = () => {
         passwordLabel.current.style.display="none";
     },[])
 
-    function CheckUserAnPassword(event){
-        if(user.current.value.length < 6){
-            userLabel.current.style.display="flex";
-            userLabel.current.style.color="var(--VibrantRed)";
-            user.current.style.border="1px solid var(--VibrantRed)";
-        }
-        else{
-            userLabel.current.style.display="none";
-            user.current.style.border="1px solid var(--SolidBlack)";
-        }
-        if(password.current.value.length < 8){
-            passwordLabel.current.style.display="flex";
-            passwordLabel.current.style.color="var(--VibrantRed)";
-            password.current.style.border="1px solid var(--VibrantRed)";
-        }
-        else{
-            passwordLabel.current.style.display="none";
-            password.current.style.border="1px solid var(--SolidBlack)";
-        }
-        event.preventDefault();
-    }
-
-    /*pegando dados em localstorage caso haja*/ 
     useEffect(()=>{
         const theUser = localStorage.getItem("userName");
         const thePassword = localStorage.getItem("userPassword");
@@ -51,6 +28,27 @@ const Enter = () => {
         user.current.value= theUser;
         password.current.value= thePassword;
     },[])
+
+    function Enter(event){
+        if(user.current.value == localStorage.getItem("userName") && password.current.value == localStorage.getItem("userPassword")){
+            console.log("entrou");
+            userLabel.current.style.display="none";
+            user.current.style.border="1px solid var(--SolidBlack)";
+            passwordLabel.current.style.display="none";
+            password.current.style.border="1px solid var(--SolidBlack)";
+        }
+        else if(user.current.value != localStorage.getItem("userName")){
+            userLabel.current.style.display="flex";
+            userLabel.current.style.color="var(--VibrantRed)";
+            user.current.style.border="1px solid var(--VibrantRed)";
+        }
+        else if(password.current.value != localStorage.getItem("userPassword")){
+            passwordLabel.current.style.display="flex";
+            passwordLabel.current.style.color="var(--VibrantRed)";
+            password.current.style.border="1px solid var(--VibrantRed)";
+        }
+        event.preventDefault();
+    }
 
     return(
         <>
@@ -60,7 +58,7 @@ const Enter = () => {
             <label htmlFor="password" ref={ passwordLabel }>senha incorreta</label>
             <input type="password" placeholder="sua senha" ref={ password }/>
             <p>esqueçeu a sua senha?</p>
-            <input type="submit" value="entrar" onClick={ CheckUserAnPassword }/>
+            <input type="submit" value="entrar" onClick={ Enter }/>
             <p onClick={()=>{enterOrRegister.setEnter(!enterOrRegister.enter)}}>não tem uma conta? Registre-se</p>
             <button type="button">entrar como convidado</button>{/*teste sem verificação de dados*/}
         </>
