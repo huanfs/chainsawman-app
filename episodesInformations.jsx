bbf67e9547c8dc6883800125c6f6bd3528506a6d
Huan Figuereido <huanfiguereido@gmail.com>
2024-05-24 10:41:38 -0300

    Updates

---
+++ episodesInformations.jsx
@@ -0,0 +1,79 @@
+//importation of each episode image cover
+import thumb_01 from "../src/assets/thumb/01.png";
+import thumb_02 from "../src/assets/thumb/02.png";
+import thumb_03 from "../src/assets/thumb/03.png";
+import thumb_04 from "../src/assets/thumb/04.png";
+import thumb_05 from "../src/assets/thumb/05.png";
+import thumb_06 from "../src/assets/thumb/06.png";
+import thumb_07 from "../src/assets/thumb/07.png";
+import thumb_08 from "../src/assets/thumb/08.png";
+import thumb_09 from "../src/assets/thumb/09.png";
+import thumb_10 from "../src/assets/thumb/10.png";
+import thumb_11 from "../src/assets/thumb/11.png";
+import thumb_12 from "../src/assets/thumb/12.png";
+//creation of one array with all covers
+export const allThumbs = [
+    thumb_01,
+    thumb_02,
+    thumb_03,
+    thumb_04,
+    thumb_05,
+    thumb_06,
+    thumb_07,
+    thumb_08,
+    thumb_09,
+    thumb_10,
+    thumb_11,
+    thumb_12,
+]
+//array with one cover and one episode label respectively
+export const episodesInformations = [
+    {
+        "cover" : allThumbs[0],
+        "episode" : "episodio 01"
+    },
+    {
+        "cover" : allThumbs[1],
+        "episode" : "episodio 02"
+    },
+    {
+        "cover" : allThumbs[2],
+        "episode" : "episodio 03"
+    },
+    {
+        "cover" : allThumbs[3],
+        "episode" : "episodio 04"
+    },
+    {
+        "cover" : allThumbs[4],
+        "episode" : "episodio 05"
+    },
+    {
+        "cover" : allThumbs[5],
+        "episode" : "episodio 06"
+    },
+    {
+        "cover" : allThumbs[6],
+        "episode" : "episodio 07"
+    },
+    {
+        "cover" : allThumbs[7],
+        "episode" : "episodio 08"
+    },
+    {
+        "cover" : allThumbs[8],
+        "episode" : "episodio 09"
+    },
+    {
+        "cover" : allThumbs[9],
+        "episode" : "episodio 10"
+    },
+    {
+        "cover" : allThumbs[10],
+        "episode" : "episodio 11"
+    },
+    {
+        "cover" : allThumbs[11],
+        "episode" : "episodio 12"
+    }
+]
