423103b0bdc88607a140423fe58f98ee60308acd
Huan Figuereido <huanfiguereido@gmail.com>
2024-05-30 16:38:25 -0300

    :computer: feat/ adicionei mais uma rota permitida dentro do arquivo server.js e coloquei no origin um array com as rotas que são permitidas

--- API/server.js
+++ API/server.js
@@ -9,8 +9,8 @@ import { CurrentUser } from "./database/model.js";
 const server = express();
 
 server.use(cors({
-    origin: 'https://huanfs.github.io/chainsawman-app',
-})); //usando o cors
+    origin: ['https://huanfs.github.io/chainsawman-app','http://localhost:3000/chainsawman-app'
+]})); //usando o cors
 
 server.use(bodyParser.json()); //usando o body parser para ler a URL
 
