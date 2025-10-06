b16d7ad9debcf4406cd53bc8d1340c847fac5577
huanfs <huanfiguereido@gmail.com>
2025-10-06 09:04:14 -0300

    :tada: add: adiciona função que lida com a autentitação no banco de dados

---
+++ src/services/auth/Authentication.js
@@ -0,0 +1,24 @@
+export async function Authentication({userName, userPassword}){
+    const user = {
+        usuario:userName,
+        senha:userPassword
+    }
+    try{
+        const auth = await fetch("http://localhost:3000/autenticar",{
+            method:"POST",
+            body: JSON.stringify(user),
+            headers:{
+                "Content-Type": "application/json",
+            }
+        });
+        if(auth.ok) return true;
+        else{
+            console.log("erro ao autenticar");
+            return false;
+        }
+    }
+    catch(err){
+        console.log("error:", err);
+        return false;
+    }
+}
