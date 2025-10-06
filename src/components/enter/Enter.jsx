0bc93106dbdf430c62112c03b96f23c479790b2d
huanfs <huanfiguereido@gmail.com>
2025-10-06 09:06:40 -0300

    :eyes: recycle: refatora o componente

--- src/components/enter/Enter.jsx
+++ src/components/enter/Enter.jsx
@@ -1,109 +1,87 @@
-import React, { useRef, useEffect } from "react";
+import React,{ useRef, useContext } from "react";
 
-import { useNavigate } from "react-router-dom"; // IMPORTAÇÃO DO HOOK NAVIGATE
+import { useNavigate } from "react-router-dom";
 
-/*context*/
-    import { GlobalContext } from "../../Context.jsx";
-/*context*/
+import { GlobalContext } from "../../Context.jsx";
 
-import "./Enter.scss"; //<--styles
+import { IsValidCredentials } from "@utils/IsValidCredentials.js";
+
+import { Authentication } from "@services/auth/Authentication.js";
+
+import "./Enter.scss";
 
 const Enter = () => {
 
-    const Navigate = useNavigate(); //PASSANDO O HOOK PARA DENTRO DE UMA VARIÁVEL
+    const Navigate = useNavigate();
 
-    const enterOptions = React.useContext(GlobalContext); //<--consuming context
+    const {
+        userName,
+        setUserName,
+        userPassword,
+        setUserPassword,
+        enter,
+        setEnter
+    } = useContext(GlobalContext);
 
-    /*referencias*/
-        const user = useRef(null);
-        const password = useRef(null);
-        const userLabel = useRef(null);
-        const passwordLabel = useRef(null);
-        const root = useRef(document.querySelector("#root"));
-    /*referencias*/
+    const userLabel = useRef(null);
+    const passwordLabel = useRef(null);
 
-    /*define as labels para display:none*/
-        useEffect(()=>{
-            userLabel.current.style.display="none";
-            passwordLabel.current.style.display="none";
-        },[])
-    /*define as labels para display:none*/
-
-    /*caso enterOptions.userName e userPsssword tenham valores definidos (após o registro)
-    define em localStorage estes valores*/
-        useEffect(()=>{
-            enterOptions.userName != null ? localStorage.setItem("userName", enterOptions.userName) : null
-            enterOptions.userPassword != null ? localStorage.setItem("userPassword", enterOptions.userPassword): null
-        },[enterOptions.userName, enterOptions.userPassword])
-    /*caso enterOptions.userName e userPsssword tenham valores definidos (após o registro)
-    define em localStorage estes valores*/
-
-    /*caso os valores referentes ao usuario e senha já existam (usuario previamente registrado)
-    define os campos de usuario e senha no componente entrar com o usuario e senha previamente registrados */ 
-        useEffect(()=>{
-            if(localStorage.getItem("userName")){
-                user.current.value =  localStorage.getItem("userName")
-            }
-            if(localStorage.getItem("userPassword")){
-                password.current.value =  localStorage.getItem("userPassword")
-            }
-        })
-    /*caso os valores referentes ao usuario e senha já existam (usuario previamente registrado)
-    define os campos de usuario e senha no componente entrar com o usuario e senha previamente registrados */
-
-    function Enter(event){
-        if(user.current.value == localStorage.getItem("userName") && password.current.value == localStorage.getItem("userPassword")){
-            userLabel.current.style.display="none";
-            user.current.style.border="1px solid var(--SolidBlack)";
-            passwordLabel.current.style.display="none";
-            password.current.style.border="1px solid var(--SolidBlack)";
-            root.current.style.animationName="slide-out-bck-center";
-            AutenticarUsuario();//chamada da função autenticar
-        }
-        else if(user.current.value != localStorage.getItem("userName")){
-            userLabel.current.style.display="flex";
-            userLabel.current.style.color="var(--VibrantRed)";
-            user.current.style.border="1px solid var(--VibrantRed)";
-        }
-        else if(password.current.value != localStorage.getItem("userPassword")){
-            passwordLabel.current.style.display="flex";
-            passwordLabel.current.style.color="var(--VibrantRed)";
-            password.current.style.border="1px solid var(--VibrantRed)";
-        }
+    async function HandleAuthentication(event){
         event.preventDefault();
-    }
-/*função de autenticar o usuário checando se o mesmo existe na tabela do banco de dados*/
-    async function AutenticarUsuario(){
-        const dados = {
-            usuario: user.current.value,
-            senha: password.current.value,
-        }
-        const Autenticar = await fetch("http://localhost:3000/autenticar",{
-            method: 'POST',
-            body: JSON.stringify(dados),
-            headers:{
-                'Content-Type':'application/json',
-            }
+        const isValid = await IsValidCredentials({
+            userName, 
+            userPassword, 
+            userLabel, 
+            passwordLabel, 
         });
-        const resultado = await Autenticar.json();
-        const timeout = setTimeout((t)=>{
-            Navigate("/mainApp"); //MUDANDO A ROTA APÓS TODO O PROCESSAMENTO
-        },900);
-        timeout();
-    }
-    /*função de autenticar o usuário checando se o mesmo existe na tabela do banco de dados*/
+        if(!isValid) return;
+        const authenticated = await Authentication({userName, userPassword});
+        if(authenticated){
+            Navigate("/mainApp");
+        }
+    };
 
     return(
         <>
             <h1>entrar</h1>
-            <label htmlFor="user" ref={ userLabel }>usuário não encontrado</label>
-            <input type="text" placeholder="nome de usuário" ref={ user} />
-            <label htmlFor="password" ref={ passwordLabel }>senha incorreta</label>
-            <input type="password" placeholder="sua senha" ref={ password } autoComplete="on"/>
+            <label 
+                htmlFor="user" 
+                ref={ userLabel }>
+                    nome de usuário:
+            </label>
+            <input 
+                type="text" 
+                placeholder="nome de usuário"
+                onChange={(e)=>setUserName(e.target.value)} 
+            />
+            <label 
+                htmlFor="password" 
+                ref={ passwordLabel }>
+                    senha:
+            </label>
+            <input 
+                type="password" 
+                placeholder="sua senha" 
+                autoComplete="on"
+                onChange={(e)=>setUserPassword(e.target.value)}
+            />
             <p>esqueçeu a sua senha?</p>
-            <input type="submit" value="entrar" onClick={ Enter }/>
-            <p onClick={()=>{enterOptions.setEnter(!enterOptions.enter)}}>não tem uma conta? Registre-se</p>
-            <button type="button" onClick={()=>Navigate("/mainApp")}>entrar como convidado</button>
+            <input 
+                type="submit" 
+                value="entrar" 
+                onClick={ (event)=>{ HandleAuthentication(event) }}
+            />
+            <p 
+            onClick={()=>{
+                setEnter(!enter)
+                }}>
+                    não tem uma conta? Registre-se
+            </p>
+            <button 
+                type="button" 
+                onClick={()=>Navigate("/mainApp")}>
+                    entrar como convidado
+            </button>
         </>
     )
 }
