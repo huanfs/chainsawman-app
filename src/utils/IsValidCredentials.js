a21b6e73f285786b708615f1c0c8e693560a7caf
huanfs <huanfiguereido@gmail.com>
2025-10-06 09:05:13 -0300

    :recycle: refactor: refatora função

--- src/utils/IsValidCredentials.js
+++ src/utils/IsValidCredentials.js
@@ -1,13 +1,11 @@
-
 export async function IsValidCredentials({
     userName, 
     userPassword, 
     userLabel, 
-    passwordLabel
+    passwordLabel,
 }) {
     let isValid = true;
 
-    // Validação do usuário
     const isUserValid = userName.length >= 6;
     if (userLabel?.current) {
         userLabel.current.innerHTML = isUserValid ? "nome de usuário:" : "mínimo 6 caracteres!";
@@ -15,19 +13,12 @@ export async function IsValidCredentials({
     }
     if (!isUserValid) isValid = false;
 
-    // Validação da senha
     const isPasswordValid = userPassword.length >= 8;
     if (passwordLabel?.current) {
         passwordLabel.current.innerHTML = isPasswordValid ? "senha:" : "mínimo 8 caracteres!";
         passwordLabel.current.style.color = isPasswordValid ? "var(--SolidBlack)" : "var(--FadeRed)";
     }
     if (!isPasswordValid) isValid = false;
-
-    
-    if(isValid){
-        console.log("validando")
-        const login = await Handler({userName,userPassword})
-    }
 
     return isValid;
 }
