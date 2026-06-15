ec076a3a638aee5051778924b0bc6e5503c9d898
huanfs <huanfiguereido@gmail.com>
2026-06-15 16:48:52 -0300

    :recycle: refactor: trata a resposta da api de forma legível e envia as mensagens para o front

--- src/services/auth/Registration.js
+++ src/services/auth/Registration.js
@@ -1,26 +1,29 @@
-
 export async function Registration({userName, userPassword}){
-     const apiUrl = import.meta.env.VITE_API_URL;
+    const apiUrl = import.meta.env.VITE_API_URL;
     const user = {
         userName: userName,
         userPassword: userPassword,
     };
         try {
         const register = await fetch(`${apiUrl}/adicionar`, {
-            method: 'POST',
-            body: JSON.stringify(user),
-            headers: {
-                'Content-Type': 'application/json',
-            },
+            method:"POST",
+            body:JSON.stringify(user),
+            headers:{
+                "Content-Type":"application/json",
+            }
         });
-        if(register.ok){
-            return true;
-        }
-        else{
-            return false;
+        const response = await register.json();
+        return {
+            success: response.ok,
+            status: response.status,
+            message: response.message,
         }
     } catch (err) {
-        console.log("Não foi possível criar usuário:", err);
-        return false;
+        consoloe.log(response.message);
+        return {
+            success: false,
+            status: 500,
+            message: "erro no servidor",
+        }
     }
 };
